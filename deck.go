bead594b681c4164194c743dc379ecfac2a33bc9
Nick Kotenberg <happilymarrieddad@gmail.com>
2020-01-03 01:37:23 -0700

    finished

--- deck.go
+++ deck.go
@@ -7,7 +7,8 @@ import (
 
 // Deck - a deck of cards
 type Deck struct {
-	cards []Card
+	cards                []Card
+	numberOfInitialCards int
 }
 
 // Build - build the deck with a series of cards
@@ -24,9 +25,20 @@ func (d *Deck) Build(numOfDecks int, possibleCards ...Card) {
 	d.cards = []Card{}
 
 	// Actually build the deck
+	d.numberOfInitialCards = 0
 	for i := 0; i < numOfDecks; i++ {
-		for j := 0; j < len(possibleCards); j++ {
-			d.cards = append(d.cards, possibleCards[j])
+
+		numberOfPossibleCardsSetsInDeck := 4
+		fullDeck := []Card{}
+		for j := 0; j < numberOfPossibleCardsSetsInDeck; j++ {
+			for k := 0; k < len(possibleCards); k++ {
+				fullDeck = append(fullDeck, possibleCards[k])
+			}
+		}
+
+		for j := 0; j < len(fullDeck); j++ {
+			d.numberOfInitialCards++
+			d.cards = append(d.cards, fullDeck[j])
 		}
 	}
 
@@ -40,4 +52,22 @@ func (d *Deck) Shuffle() {
 		randIndex := r.Intn(n)
 		d.cards[n-1], d.cards[randIndex] = d.cards[randIndex], d.cards[n-1]
 	}
+}
+
+func (d *Deck) GetCard() (x Card) {
+	x, d.cards = d.cards[0], d.cards[1:]
+	return
+}
+
+func (d *Deck) GetNumberOfCards() int {
+	return len(d.cards)
+}
+
+func (d *Deck) GetCopyOfDeck() []Card {
+	return d.cards
+}
+
+// PercentageLeftInDeck - return the percentage of the deck that is left
+func (d *Deck) PercentageLeftInDeck() float32 {
+	return (float32(len(d.cards)) / float32(d.numberOfInitialCards)) * 100
 }
--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,8 @@
 package cards
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_Deck_Build(t *testing.T) {
 	var d Deck
@@ -20,4 +22,124 @@ func Test_Deck_Build_Test_No_Decks(t *testing.T) {
 	var d Deck
 
 	d.Build(0)
+}
+
+func Test_Deck_PercentageLeft(t *testing.T) {
+	var d Deck
+
+	d.Build(1)
+	d.Shuffle()
+
+	deck := d.GetCopyOfDeck()
+
+	numberOf2s := 0
+	numberOf3s := 0
+	numberOf4s := 0
+	numberOf5s := 0
+	numberOf6s := 0
+	numberOf7s := 0
+	numberOf8s := 0
+	numberOf9s := 0
+	numberOf10s := 0
+	numberOfJs := 0
+	numberOfQs := 0
+	numberOfKs := 0
+	numberOfAs := 0
+
+	for _, c := range deck {
+		switch c.val {
+		case "2":
+			numberOf2s++
+		case "3":
+			numberOf3s++
+		case "4":
+			numberOf4s++
+		case "5":
+			numberOf5s++
+		case "6":
+			numberOf6s++
+		case "7":
+			numberOf7s++
+		case "8":
+			numberOf8s++
+		case "9":
+			numberOf9s++
+		case "10":
+			numberOf10s++
+		case "J":
+			numberOfJs++
+		case "Q":
+			numberOfQs++
+		case "K":
+			numberOfKs++
+		case "A":
+			numberOfAs++
+		}
+	}
+
+	if numberOf2s != 4 {
+		t.Error("numberOf2s not equal to 4")
+		return
+	}
+	if numberOf3s != 4 {
+		t.Error("numberOf3s not equal to 4")
+		return
+	}
+	if numberOf4s != 4 {
+		t.Error("numberOf4s not equal to 4")
+		return
+	}
+	if numberOf5s != 4 {
+		t.Error("numberOf5s not equal to 4")
+		return
+	}
+	if numberOf6s != 4 {
+		t.Error("numberOf6s not equal to 4")
+		return
+	}
+	if numberOf7s != 4 {
+		t.Error("numberOf7s not equal to 4")
+		return
+	}
+	if numberOf8s != 4 {
+		t.Error("numberOf8s not equal to 4")
+		return
+	}
+	if numberOf9s != 4 {
+		t.Error("numberOf9s not equal to 4")
+		return
+	}
+	if numberOf10s != 4 {
+		t.Error("numberOf10s not equal to 4")
+		return
+	}
+	if numberOfJs != 4 {
+		t.Error("numberOfJs not equal to 4")
+		return
+	}
+	if numberOfQs != 4 {
+		t.Error("numberOfQs not equal to 4")
+		return
+	}
+	if numberOfKs != 4 {
+		t.Error("numberOfKs not equal to 4")
+		return
+	}
+	if numberOfAs != 4 {
+		t.Error("numberOfAs not equal to 4")
+		return
+	}
+
+	// Ignore the returned card
+	d.GetCard()
+
+	if float32(98.07692) != d.PercentageLeftInDeck() {
+		t.Error("bad percentage left")
+		return
+	}
+
+	if 51 != d.GetNumberOfCards() {
+		t.Error("bad number of cards left")
+		return
+	}
 }
